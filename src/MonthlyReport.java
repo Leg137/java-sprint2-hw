f1d03a9395bb20cd0cec9fc723dd9331f99f401f
MrOlegBar <Legan26021991@yandex.ru>
2022-04-13 17:55:56 +0300

    Записал все данные в HashMap

--- src/MonthlyReport.java
+++ src/MonthlyReport.java
@@ -1,45 +1,75 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class MonthlyReport {
     String pathCatalogBy_Months = ("Reports/by months");
     File catalogBy_Months = new File(pathCatalogBy_Months);
+    HashMap<String, HashMap<String, String[]>> monthlyReportsMaps = new HashMap<>();
+    BufferedReader reader;
+    String line;
+    String keyFileName;
+    HashMap<String, String[]> monthlyReportMap;
 
     void readingMonthlyReports() {
         try {
-            File[] abstractPathsFiles = catalogBy_Months.listFiles(new FilenameFilter() {
-                @Override
-                public boolean accept(File dir, String name) {
-                    return name.matches("m.\\d{6}.csv");
-                }
-            });
+            File[] abstractPathsFiles = catalogBy_Months.listFiles((dir, name) -> name.matches("m.\\d{6}.csv"));
+
             if (abstractPathsFiles != null && abstractPathsFiles.length != 0) {
-                for (File abstractPathFile : abstractPathsFiles) {
-                    System.out.println(abstractPathFile);
-                    BufferedReader reader = new BufferedReader(new FileReader(abstractPathFile));
-                    String line;
+
+                for (File abstractPathsFile : abstractPathsFiles) {
+                    //System.out.println("\n" + abstractPathsFile.getName() + "\n");
+
+                    keyFileName = abstractPathsFile.getName();
+                    monthlyReportMap = new HashMap<>();
+                    String[] massivLines;
+                    String keyItemName;
+                    reader = new BufferedReader(new FileReader(abstractPathsFile));
+
                     while ((line = reader.readLine()) != null) {
-                        System.out.println(line);
+                        //System.out.println(line);
+
+                        massivLines = line.split(",");
+                        keyItemName = massivLines[0];
+                        monthlyReportMap.put(keyItemName, massivLines);
+                        monthlyReportsMaps.put(keyFileName, monthlyReportMap);
                     }
                     reader.close();
                 }
+
+
             } else {
-                System.out.println(
-                        "Ошибка: файл не найден\n" +
+                System.out.println("Равно null или 0");
+                windowsError();
+            }
+        } catch (IOException e) {
+            windowsError();
+        }
+
+        for (String keyMonthlyReportsMaps : monthlyReportsMaps.keySet()) {
+            monthlyReportMap = monthlyReportsMaps.get(keyMonthlyReportsMaps);
+            System.out.println("\n" + keyMonthlyReportsMaps + "\n");
+            for (String keyMonthlyReportMap : monthlyReportMap.keySet()) {
+                //System.out.println("\n" + keyMonthlyReportMap + "\n");
+                String[] itemName = monthlyReportMap.get(keyMonthlyReportMap);
+                for (String item : itemName) {
+                    System.out.println(item);
+                }
+
+            }
+        }
+
+    }
+
+    void windowsError() {
+        System.out.println(
+                "Ошибка: файл не найден\n" +
                         "⣿⠀⠀⢀⣴⣾⣿⣿⣿⣿⣦⡀⠀⠀⣿ Возможно файл в формате \"m.YYYYMM.csv\" не находится в папке:\n" +
                         "⣿⠀⢠⣿⣿⡉⠙⠿⠋⢉⣻⣿⡄⠀⣿ \"" + catalogBy_Months.getAbsolutePath() + "\"\n" +
                         "⣿⠀⢸⣿⣿⣿⠆⠀⠰⣿⣿⣿⡇⠀⣿   m — буква в начале файла отделяет отчёты за месяц и отчёты за год;\n" +
                         "⣿⠀⠸⣿⣿⣄⣴⣿⣦⣀⣽⣿⠏⠀⣿YYYY — год. Например, 2022;\n" +
                         "⣿⠀⠀⠙⢿⣿⣿⣿⣿⣿⡿⠋⠀⠀⣿  MM — месяц строго 2мя цифрами.");
-            }
-        } catch (Exception e) {
-            System.out.println(
-                    "Ошибка: файл не найден\n" +
-                            "⣿⠀⠀⢀⣴⣾⣿⣿⣿⣿⣦⡀⠀⠀⣿ Возможно файл в формате \"m.YYYYMM.csv\" не находится в папке:\n" +
-                            "⣿⠀⢠⣿⣿⡉⠙⠿⠋⢉⣻⣿⡄⠀⣿ \"" + catalogBy_Months.getAbsolutePath() + "\"\n" +
-                            "⣿⠀⢸⣿⣿⣿⠆⠀⠰⣿⣿⣿⡇⠀⣿   m — буква в начале файла отделяет отчёты за месяц и отчёты за год;\n" +
-                            "⣿⠀⠸⣿⣿⣄⣴⣿⣦⣀⣽⣿⠏⠀⣿YYYY — год. Например, 2022;\n" +
-                            "⣿⠀⠀⠙⢿⣿⣿⣿⣿⣿⡿⠋⠀⠀⣿  MM — месяц строго 2мя цифрами.");
-        }
-
     }
 }
